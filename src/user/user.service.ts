70f261670bf56a6d71e4be7a8dbf50b62a87b685
Alex Marcelle <axmfs@outlook.com>
2024-12-20 20:20:28 -0300

    added missing routes in user service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,20 +1,46 @@
-import { Injectable } from "@nestjs/common";
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { UserRepository } from "./user.repository";
 import { CreateUserDto } from "./dto/create-user.dto";
-import { bcrypt } from "bcrypt";
-import { IUser } from "./user.interface";
+import * as bcrypt from "bcrypt";
 
 @Injectable()
 export class UserService {
   constructor(private readonly userRepository: UserRepository) {}
 
   async createUser(user: CreateUserDto) {
-    const hash = await bcrypt.hash(user.password, 18);
-    user.password = hash;
+    user.password = await bcrypt.hash(user.password, 18);
     return this.userRepository.createUser(user);
   }
 
-  async getUser(id: string): Promise<IUser> {
-    return this.userRepository.getUserById(id);
+  async getUser(id: string) {
+    const user = await this.userRepository.getUserById(id);
+
+    if(!user || user == null) {
+      throw new HttpException("No user found with that id", HttpStatus.NOT_FOUND);
+    }
+
+    return user;
+  }
+
+  async deleteUser(id: string) {
+    const user = await this.userRepository.getUserById(id);
+    if(!user) {
+      throw new HttpException("No user found with that id", HttpStatus.NOT_FOUND);
+    }
+
+    await this.userRepository.deleteUser(id);
+    
+    const checkUser = await this.userRepository.getUserById(id);
+    if(checkUser) {
+      throw new HttpException(`Failed to delete user with id ${id}`, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    return {
+      message: `User with id ${id} deleted successfully`
+    };
+  }
+
+  async listUsers() {
+    return this.userRepository.listUsers();
   }
 }
