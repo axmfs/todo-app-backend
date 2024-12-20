0fd44e758dccedae57812b510302eb1187006f0d
Alex Marcelle <axmfs@outlook.com>
2024-12-20 20:19:25 -0300

    missing user routes (list, create, delete)

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,28 @@
-import { Controller, Get, Param } from "@nestjs/common";
-import { IUser } from "./user.interface";
+import { Body, Controller, Delete, Get, Param, Post } from "@nestjs/common";
 import { UserService } from "./user.service";
+import { CreateUserDto } from "./dto/create-user.dto";
 
 @Controller("/users")
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @Get()
+  async listUsers() {
+    return this.userService.listUsers();
+  }
+
   @Get(":id")
-  async getUser(@Param() params: any): Promise<IUser> {
+  async getUser(@Param() params: any) {
     return this.userService.getUser(params.id);
+  }
+
+  @Post()
+  async createUser(@Body() data: CreateUserDto) {
+    return this.userService.createUser(data);
+  }
+
+  @Delete(":id")
+  async deleteUser(@Param() params: any) {
+    return this.userService.deleteUser(params.id);
   }
 }
